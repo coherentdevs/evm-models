7874289e9eb6c7e10de2ed74a8c76d161bbf6218
Eugene Tian <eugene12551@gmail.com>
2023-06-09 10:47:28 -0400

    polygon dbt models

---
+++ models/polygon/decoded_polygon_transactions.sql
@@ -0,0 +1,216 @@
+{{ config(materialized='incremental', unique_key='transaction_hash', merge_update_columns = ['hashable_signature', 'decoded_input'], cluster_by=['from_address']) }}
+
+{% if is_incremental() %}
+    {% if not var('backfill') %} -- if not backfilling
+        WITH input_and_transaction_polygon AS (
+            SELECT
+                TRY_CAST(hex_to_int(BLOCK_NUMBER) as FLOAT) as BLOCK_NUMBER,
+                BLOCK_HASH,
+                TRY_CAST(hex_to_int(CUMULATIVE_GAS_USED) as FLOAT) as CUMULATIVE_GAS_USED,
+                FROM_ADDRESS,
+                TRY_CAST(hex_to_int(GAS) as FLOAT) as GAS,
+                TRY_CAST(hex_to_int(GAS_PRICE) as FLOAT) as GAS_PRICE,
+                TRY_CAST(hex_to_int(GAS_USED) as FLOAT) as GAS_USED,
+                TRY_CAST(hex_to_int(EFFECTIVE_GAS_PRICE) as FLOAT) as EFFECTIVE_GAS_PRICE,
+                TRY_CAST(hex_to_int(MAX_FEE_PER_GAS) as FLOAT) as MAX_FEE_PER_GAS,
+                TRY_CAST(hex_to_int(MAX_PRIORITY_FEE_PER_GAS) as FLOAT) as MAX_PRIORITY_FEE_PER_GAS,
+                INPUT,
+                LOGS_BLOOM,
+                TRY_CAST(hex_to_int(NONCE) as NUMBER) as NONCE,
+                R,
+                S,
+                True as Status,
+                TRY_CAST(hex_to_int(TYPE) as FLOAT) as TYPE,
+                TO_ADDRESS,
+                TRANSACTION_HASH,
+                TRY_CAST(hex_to_int(TRANSACTION_INDEX) as NUMBER) as TRANSACTION_INDEX,
+                V,
+                TRY_CAST(hex_to_int(VALUE) as FLOAT) as VALUE,
+                ACCESS_LIST,
+                SUBSTRING(INPUT, 0, 10) AS METHOD_HEADER
+            FROM {{ source(var('polygon_raw_database'), 'transactions') }}
+            WHERE STATUS = '0x1' AND to_number(SUBSTR(block_number, 3), repeat('X', length(SUBSTR(block_number, 3))))  > (SELECT MAX(block_number) FROM {{ this }}) -- this is the only change
+        ),
+    {% else %} -- backfilling
+        WITH input_and_transaction_polygon AS (
+            SELECT
+                BLOCK_NUMBER,
+                BLOCK_HASH,
+                CUMULATIVE_GAS_USED,
+                FROM_ADDRESS,
+                GAS,
+                GAS_PRICE,
+                GAS_USED,
+                EFFECTIVE_GAS_PRICE,
+                MAX_FEE_PER_GAS,
+                MAX_PRIORITY_FEE_PER_GAS,
+                INPUT,
+                LOGS_BLOOM,
+                NONCE,
+                R,
+                S,
+                STATUS,
+                TYPE,
+                TO_ADDRESS,
+                TRANSACTION_HASH,
+                TRANSACTION_INDEX,
+                V,
+                VALUE,
+                ACCESS_LIST,
+                SUBSTRING(INPUT, 0, 10) AS METHOD_HEADER
+            FROM {{ source(var('polygon_decoded_database'), 'decoded_polygon_transactions') }}
+            WHERE DECODED_INPUT is NULL AND INPUT != '0x'
+        ),
+    {% endif %}
+{% else %} -- full refresh
+    WITH input_and_transaction_polygon AS (
+        SELECT
+            TRY_CAST(hex_to_int(BLOCK_NUMBER) as FLOAT) as BLOCK_NUMBER,
+            BLOCK_HASH,
+            TRY_CAST(hex_to_int(CUMULATIVE_GAS_USED) as FLOAT) as CUMULATIVE_GAS_USED,
+            FROM_ADDRESS,
+            TRY_CAST(hex_to_int(GAS) as FLOAT) as GAS,
+            TRY_CAST(hex_to_int(GAS_PRICE) as FLOAT) as GAS_PRICE,
+            TRY_CAST(hex_to_int(GAS_USED) as FLOAT) as GAS_USED,
+            TRY_CAST(hex_to_int(EFFECTIVE_GAS_PRICE) as FLOAT) as EFFECTIVE_GAS_PRICE,
+            TRY_CAST(hex_to_int(MAX_FEE_PER_GAS) as FLOAT) as MAX_FEE_PER_GAS,
+            TRY_CAST(hex_to_int(MAX_PRIORITY_FEE_PER_GAS) as FLOAT) as MAX_PRIORITY_FEE_PER_GAS,
+            INPUT,
+            LOGS_BLOOM,
+            TRY_CAST(hex_to_int(NONCE) as NUMBER) as NONCE,
+            R,
+            S,
+            True as Status,
+            TRY_CAST(hex_to_int(TYPE) as FLOAT) as TYPE,
+            TO_ADDRESS,
+            TRANSACTION_HASH,
+            TRY_CAST(hex_to_int(TRANSACTION_INDEX) as NUMBER) as TRANSACTION_INDEX,
+            V,
+            TRY_CAST(hex_to_int(VALUE) as FLOAT) as VALUE,
+            ACCESS_LIST,
+            SUBSTRING(INPUT, 0, 10) AS METHOD_HEADER
+        FROM {{ source(var('polygon_raw_database'), 'transactions') }}
+        WHERE STATUS = '0x1' -- only interested in successful transactions
+    ),
+{% endif %}
+
+merged AS (
+    SELECT DISTINCT
+        t.*,
+        m.METHOD_ID,
+        m.hashable_signature,
+        m.abi
+    FROM input_and_transaction_polygon t
+    LEFT JOIN {{ source(var('contracts_database'), 'method_fragments') }} m
+        ON t.METHOD_HEADER = m.METHOD_ID
+),
+
+decoded_transactions AS (
+    SELECT
+        BLOCK_NUMBER,
+        BLOCK_HASH,
+        CUMULATIVE_GAS_USED,
+        FROM_ADDRESS,
+        GAS,
+        GAS_PRICE,
+        GAS_USED,
+        EFFECTIVE_GAS_PRICE,
+        MAX_FEE_PER_GAS,
+        MAX_PRIORITY_FEE_PER_GAS,
+        INPUT,
+        LOGS_BLOOM,
+        NONCE,
+        R,
+        S,
+        STATUS,
+        TYPE,
+        TO_ADDRESS,
+        TRANSACTION_HASH,
+        TRANSACTION_INDEX,
+        V,
+        VALUE,
+        ACCESS_LIST,
+        HASHABLE_SIGNATURE,
+        decode_input(abi, SUBSTRING(INPUT,11))[0] as decoded_result,
+        decode_input(abi, SUBSTRING(INPUT,11))[1] as decoded_success
+    FROM merged
+    WHERE ABI IS NOT NULL
+),
+
+decoded_cleaned AS (
+    SELECT
+        BLOCK_NUMBER,
+        BLOCK_HASH,
+        CUMULATIVE_GAS_USED,
+        FROM_ADDRESS,
+        GAS,
+        GAS_PRICE,
+        GAS_USED,
+        EFFECTIVE_GAS_PRICE,
+        MAX_FEE_PER_GAS,
+        MAX_PRIORITY_FEE_PER_GAS,
+        INPUT,
+        LOGS_BLOOM,
+        NONCE,
+        R,
+        S,
+        STATUS,
+        TYPE,
+        TO_ADDRESS,
+        TRANSACTION_HASH,
+        TRANSACTION_INDEX,
+        V,
+        VALUE,
+        ACCESS_LIST,
+        HASHABLE_SIGNATURE,
+        CASE
+            WHEN decoded_success = True THEN decoded_result
+            ELSE NULL
+        END AS DECODED_INPUT
+    FROM decoded_transactions
+),
+
+no_abi AS (
+    SELECT
+        BLOCK_NUMBER,
+        BLOCK_HASH,
+        CUMULATIVE_GAS_USED,
+        FROM_ADDRESS,
+        GAS,
+        GAS_PRICE,
+        GAS_USED,
+        EFFECTIVE_GAS_PRICE,
+        MAX_FEE_PER_GAS,
+        MAX_PRIORITY_FEE_PER_GAS,
+        INPUT,
+        LOGS_BLOOM,
+        NONCE,
+        R,
+        S,
+        STATUS,
+        TYPE,
+        TO_ADDRESS,
+        TRANSACTION_HASH,
+        TRANSACTION_INDEX,
+        V,
+        VALUE,
+        ACCESS_LIST,
+        NULL AS HASHABLE_SIGNATURE,
+        NULL AS DECODED_INPUT
+    FROM merged
+    WHERE ABI IS NULL
+),
+
+{% if not var('backfill') %} -- if not backfilling
+    all_transactions AS (
+        SELECT * FROM decoded_cleaned
+        UNION ALL
+        SELECT * FROM no_abi
+    )
+{% else %} -- backfilling
+    all_transactions AS (
+        SELECT * FROM decoded_cleaned WHERE DECODED_INPUT IS NOT NULL
+    )
+{% endif %}
+
+SELECT * FROM all_transactions
